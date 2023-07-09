0848147387e3ec8bb7471d0297173eb1e80c722d
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-07-09 12:37:57 +0300

    Added models, controllers, routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,10 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
-import NoteModel from "./models/note";
+import getNotes from "./routes/notes";
 
 const app = express();
 
-app.get("/", async (req, res, next) => {
-    try {
-        const notes = await NoteModel.find().exec();
-        res.status(200).json(notes);
-    } catch (error) {
-        next(error);
-    }
-});
+app.use('/api/notes', getNotes)
 
 app.use((req, res, next) => {
     next(Error("Endpoint not found."));
