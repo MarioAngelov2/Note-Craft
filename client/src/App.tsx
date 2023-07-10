737b35ef90d3f38d9022d3fa3e0328feaac82727
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-07-10 14:00:22 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState, useEffect } from "react";
 import { Note as NoteMotel } from "./models/note";
 import { getNotes } from "./services/requester";
 import { Notes } from "./components/Notes";
+import { Container, Row, Col } from "react-bootstrap";
+import styles from "./styles/NotesPage.module.css";
 
 function App() {
     const [notes, setNotes] = useState<NoteMotel[]>([]);
@@ -13,11 +16,15 @@ function App() {
     }, []);
 
     return (
-        <div>
-            {notes.map((note) => (
-                <Notes note={note} key={note._id} />
-            ))}
-        </div>
+        <Container fluid="xl">
+            <Row xs={1} md={2} xl={3} xxl={4} className="g-4">
+                {notes.map((note) => (
+                    <Col key={note._id}>
+                        <Notes note={note} className={styles.note}/>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
     );
 }
 
