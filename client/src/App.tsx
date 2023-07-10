7c4a67631449778206c36327970cc3a8ab3cd51d
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-07-10 11:12:41 +0300

    Render the Notes component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,24 @@
-import './App.css'
+import { useState, useEffect } from "react";
+import { Note as NoteMotel } from "./models/note";
+import { getNotes } from "./services/requester";
+import { Notes } from "./components/Notes";
 
 function App() {
+    const [notes, setNotes] = useState<NoteMotel[]>([]);
 
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  )
+    useEffect(() => {
+        getNotes().then((res) => {
+            setNotes(res);
+        });
+    }, []);
+
+    return (
+        <div>
+            {notes.map((note) => (
+                <Notes note={note} key={note._id} />
+            ))}
+        </div>
+    );
 }
 
-export default App
+export default App;
