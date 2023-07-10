b226e7c6186594fe5cb1511a12eb4e7f6922a91f
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-07-10 14:00:24 +0300

    Update Notes.tsx

--- client/src/components/Notes.tsx
+++ client/src/components/Notes.tsx
@@ -1,21 +1,30 @@
-import * as React from "react";
 import styles from "../styles/Note.module.css";
 import { Note as NoteModel } from "../models/note";
 import { Card } from "react-bootstrap";
+import { formatDate } from "../utils/formatDate";
 
 export interface NotesProps {
     note: NoteModel;
+    className?: string;
 }
 
-export function Notes({ note }: NotesProps) {
+export function Notes({ note, className }: NotesProps) {
     const { title, text, _id, createdAt, updatedAt } = note;
 
+    let createdUpdatedText: string;
+    if (updatedAt > createdAt) {
+        createdUpdatedText = "Updated: " + formatDate(updatedAt);
+    } else {
+        createdUpdatedText = "Created: " + formatDate(createdAt);
+    }
+
     return (
-        <Card className={styles.noteCard}>
-            <Card.Body>
+        <Card className={`${styles.noteCard} ${className}`}>
+            <Card.Body className={styles.cardBody}>
                 <Card.Title>{title}</Card.Title>
                 <Card.Text className={styles.cardText}>{text}</Card.Text>
             </Card.Body>
+            <Card.Footer className="text-muted">{createdUpdatedText}</Card.Footer>
         </Card>
     );
 }
