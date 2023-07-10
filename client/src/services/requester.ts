4614470f72d77939795351d6b5658dad4c5ddb73
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-07-10 15:48:13 +0300

    Update requester.ts

--- client/src/services/requester.ts
+++ client/src/services/requester.ts
@@ -1,21 +1,37 @@
+import { Note } from "../models/note";
+
 const URL = `http://localhost:5001/api/notes/`;
 
-export async function getNotes() {
-    try {
-        const response = await fetch(URL, { method: "GET" });
+async function fetchData(input: RequestInfo, init?: RequestInit) {
+    const response = await fetch(input, init);
 
-        if (response.status === 200) {
-            return response.json();
-        } else if (response.status === 204) {
-            console.error("No content");
-            return null;
-        } else if (response.status === 400) {
-            throw new Error("Bad request");
-        } else if (response.status === 404) {
-            throw new Error("Notes not found");
-        }
-    } catch (error) {
-        console.log(error);
-        throw new Error("Failed to fetch notes");
+    if (response.ok) {
+        return response;
+    } else {
+        const errorBody = await response.json();
+        const errorMessage = errorBody.error;
+        throw Error(errorMessage);
     }
+}
+
+export async function fetchNotes(): Promise<Note[]> {
+    const response = await fetchData(URL, { method: "GET" });
+    return response.json();
+}
+
+export interface NoteInput {
+    title: string;
+    text?: string;
+}
+
+export async function createNote(note: NoteInput) {
+    const response = await fetchData(URL, 
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(note),
+        })
+        return response.json();
 }
